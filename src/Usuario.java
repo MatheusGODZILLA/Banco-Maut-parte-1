59f5cb6e1aa33646738cfb7fdbe1039d63c3b816
Matheus da Silva <contagodzillajapan@gmail.com>
2023-10-02 15:09:45 -0500

    Desenvolvendo a classe Usuário

---
+++ src/Usuario.java
@@ -0,0 +1,31 @@
+private class Usuario{
+    private String email;
+    private String senha;
+    private Cliente cliente;
+
+    public Usuario(String email, String senha, Cliente cliente){
+        this.email = email;
+        this.senha = senha;
+        this.cliente = cliente;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+}
