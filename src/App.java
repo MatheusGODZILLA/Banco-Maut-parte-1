b367f8baa7902d203c0dae27649189fe45ef3d79
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-10-03 01:14:18 -0300

    Retrabalhando a classe App

--- src/App.java
+++ src/App.java
@@ -1,91 +1,106 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Cliente cliente1 = new Cliente("Matheus", "123.456.789-00", "21/06/2003", "Rua A, 123");
-        //Cliente cliente2 = new Cliente("Pedro Henrique", "987.654.321-00", "15/05/2002", "Avenida B, 456");
+        // Criação do cliente
+        System.out.println("Bem-vindo ao banco. Por favor, insira seus dados para criar uma conta:");
+        System.out.print("Nome: ");
+        String nome = scanner.nextLine();
+        System.out.print("CPF: ");
+        String cpf = scanner.nextLine();
+        System.out.print("Data de Nascimento: ");
+        String dataNascimento = scanner.nextLine();
+        System.out.print("Endereço: ");
+        String endereco = scanner.nextLine();
 
-        Conta conta1 = new Conta("001", "12345", 0, cliente1);
-        //Conta conta2 = new Conta("002", "67890", 0, cliente2);
+        Cliente cliente = new Cliente(nome, cpf, dataNascimento, endereco);
+        Conta conta = new Conta("001", "001", 0, cliente);
 
-        //conta1.depositar(1000.0);
-        //conta2.depositar(500.0);
+        while (true) {
+            System.out.println("\nEscolha uma opção:");
+            System.out.println("1. Exibir informações");
+            System.out.println("2. Ver Saldo");
+            System.out.println("3. Depositar");
+            System.out.println("4. Sacar");
+            System.out.println("5. Transferir");
+            System.out.println("6. Exibir Extrato");
+            System.out.println("7. Alterar Dados");
+            System.out.println("8. Sair");
+            System.out.print("\nOpção: ");
+            int opcao = scanner.nextInt();
+            scanner.nextLine(); // Limpa a quebra de linha
 
-        //conta1.sacar(200.0);
-
-        //conta1.transferir(conta2, 300.0);
-
-        //System.out.println("Saldo da conta de " + cliente1.getNome() + ": R$" + conta1.getSaldo());
-        //System.out.println("Saldo da conta de " + cliente2.getNome() + ": R$" + conta2.getSaldo());
-
-        System.out.println("//-- Aplicativo de Conta Bancária --//");
-
-        menu(conta1, scanner);
-
-        scanner.close();
-    }
-
-    public static void menu(Conta conta, Scanner scanner) {
-        System.out.println("1. Informações");
-        System.out.println("2. Depósito");
-        System.out.println("3. Saque");
-        System.out.println("4. Transferência");
-        System.out.println("5. Sair do app");
-
-        System.out.print("\nSelecione uma opção: ");
-        int opcao = scanner.nextInt();
-
-        switch (opcao) {
-            case 1:
-                System.out.println("Informações do Cliente:");
-                System.out.println("Nome: " + conta.getCliente().getNome());
-                System.out.println("CPF: " + conta.getCliente().getCPF());
-                System.out.println("Data de Nascimento: " + conta.getCliente().getDataNascimento());
-                System.out.println("Endereço: " + conta.getCliente().getEndereco());
-
-                System.out.println("\nInformações da Conta:");
-                System.out.println("Saldo da conta de: R$" + conta.getSaldo());
-                System.out.println("\n");
-                menu(conta, scanner);
-                break;
-
-            case 2:
-                System.out.print("\nInforme o valor para depósito: ");
-                double valorDeposito = scanner.nextDouble();
-                conta.depositar(valorDeposito);
-                menu(conta, scanner);
-                break;
-
-            case 3:
-                System.out.print("\nInforme o valor para saque: ");
-                double valorSaque = scanner.nextDouble();
-                conta.sacar(valorSaque);
-                menu(conta, scanner);
-                break;
-
-            case 4:
-                //System.out.print("Número da Conta de Destino: ");
-                //int numeroContaDestino = scanner.nextInt();
-                Cliente cliente2 = new Cliente("Pedro Henrique", "987.654.321-00", "15/05/2002", "Avenida B, 456");
-                Conta conta2 = new Conta("002", "67890", 0, cliente2);
-                if (conta2 != null) {
-                    System.out.print("\nInforme o valor para transferência: ");
-                    double valorTransferencia = scanner.nextDouble();
-                    conta.transferir(conta2, valorTransferencia);
-                    menu(conta, scanner);
-                }
-                break;
-
-                case 5:
-                    System.out.println("\nValeu!");
-                    scanner.close();
+            switch (opcao) {
+                case 1:
+                    System.out.println("\nInformações do Cliente e da Conta:");
+                    System.out.println("Nome: " + cliente.getNome());
+                    System.out.println("CPF: " + cliente.getCPF());
+                    System.out.println("Data de Nascimento: " + cliente.getDataNascimento());
+                    System.out.println("Endereço: " + cliente.getEndereco());
+                    System.out.println("Número da Agência: " + conta.getNumeroAgencia());
+                    System.out.println("Número da Conta: " + conta.getNumeroConta());
+                    System.out.println("Saldo: R$" + conta.getSaldo());
                     break;
 
+                case 2:
+                    System.out.println("\nSaldo: R$" + conta.getSaldo());
+                    break;
+
+                case 3:
+                    System.out.print("\nValor a depositar: R$");
+                    double valorDeposito = scanner.nextDouble();
+                    conta.depositar(valorDeposito);
+                    break;
+
+                case 4:
+                    System.out.print("\nValor a sacar: R$");
+                    double valorSaque = scanner.nextDouble();
+                    conta.sacar(valorSaque);
+                    break;
+
+                case 5:
+                    System.out.print("\nNúmero da Agência de Destino: ");
+                    String numeroAgenciaDestino = scanner.nextLine();
+                    System.out.print("Número da Conta de Destino: ");
+                    String numeroContaDestino = scanner.nextLine();
+                    
+                    Conta destino = new Conta(numeroAgenciaDestino, numeroContaDestino);
+                
+                    System.out.print("Valor a transferir: R$");
+                    double valorTransferencia = scanner.nextDouble();
+                    conta.transferir(destino, valorTransferencia);
+                    break;
+
+                case 6:
+                    System.out.println("\nExtrato de Transações:");
+                    for (String transacao : conta.obterTransacoes()) {
+                        System.out.println(transacao);
+                    }
+                    break;
+
+                case 7:
+                    System.out.print("\nNovo Nome: ");
+                    String novoNome = scanner.nextLine();
+                    cliente.setNome(novoNome);
+                    System.out.print("Nova Data de Nascimento: ");
+                    String novoDataNasc = scanner.nextLine();
+                    cliente.setDataNascimento(novoDataNasc);
+                    System.out.print("Novo Endereço: ");
+                    String novoEndereco = scanner.nextLine();
+                    cliente.setEndereco(novoEndereco);
+                    System.out.println("Informações atualizadas com sucesso.");
+                    break;
+
+                case 8:
+                    System.out.println("Até logo!");
+                    System.exit(0);
+                    scanner.close();
+                    break;
                 default:
-                System.out.println("\nOpção inválida. Tente novamente.");
-                menu(conta, scanner);
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
         }
     }
 }
