1ca100b821157ef0754255f84cc3fd9148f8154a
MatheusGODZILLA <contagodzillajapan@gmail.com>
2023-10-03 01:10:49 -0300

    Aprimorando a relação conta-cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,14 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente {
     private String nome;
     private String CPF;
     private String dataNascimento;
     private String endereco;
+    private List<Conta> contas; // Lista de contas associadas ao cliente
 
     public Cliente(String nome, String CPF, String dataNascimento, String endereco){
         this.nome = nome;
         this.CPF = CPF;
         this.dataNascimento = dataNascimento;
         this.endereco = endereco;
+        this.contas = new ArrayList<>();
     }
 
     public String getNome() {
@@ -27,11 +32,23 @@ public class Cliente {
         return dataNascimento;
     }
 
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
     public String getEndereco() {
         return endereco;
     }
 
     public void setEndereco(String endereco) {
         this.endereco = endereco;
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+
+    public void adicionarConta(Conta conta) {
+        contas.add(conta);
     }
 }
